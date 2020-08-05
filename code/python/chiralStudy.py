aec4f78a94045601af3134365954061982112674
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-08-05 17:24:49 +0300

    computes proper chiral potential. returns the plot as a tuple of I and theta

--- code/python/chiralStudy.py
+++ code/python/chiralStudy.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy as sp
+import scipy.special as sp
 
 # Document used for plotting the values of the triaxial potential for a fixed coupling angle
 # The implementation will plot V(q) and V_chiral(q), where the latter is computed by applying a chiral transformation on the first value of theta
@@ -19,23 +19,63 @@ def j_AM(j, theta):
 
 
 def A(I, I1, I2, j, theta):
-    return 'A'
+    A1=MOI(I1)
+    A2=MOI(I2)
+    j2=j_AM(j,theta)[1]
+    return A2*(1.0-(j2/I))-A1
 
 
 def v0(I, I1, I2, j, theta):
-    return 'v0'
+    A1=MOI(I1)
+    j1=j_AM(j,theta)[0]
+    return (-1.0)*(A1*j1)/A(I,I1,I2,j,theta)
 
 
 def u(I, I1, I2, I3, j, theta):
-    return 'u'
+    A1=MOI(I1)
+    A3=MOI(I3)
+    return (A3-A1)/A(I,I1,I2,j,theta)
+
+#study the difference of the two inertia factors involved in this function
+def U_DIFF(I, I1, I2, I3, j, theta):
+    A1=MOI(I1)
+    A3=MOI(I3)
+    return [(A3-A1)/A(I,I1,I2,j,theta),A3-A1]
 
 
 def k(I, I1, I2, I3, j, theta):
-    return 'k'
+    uparam=u(I,I1,I2,I3,j,theta)
+    if(uparam>=0.0):
+        return np.sqrt(uparam)
+    return np.sqrt(abs(uparam))
 
+
+#storing the value of the Jacobi Amplitude
+def JA(q,m):
+    m2=np.power(m,2)
+    jacobi=sp.ellipj(q,m2)[3]
+    return jacobi
+
+def SN(q,m):
+    return np.sin(JA(q,m))
+
+def CN(q,m):
+    return np.cos((JA(q,m)))
+
+def DN(q,m):
+    s=SN(q,m)
+    return np.sqrt(1.0-m*m*s*s)
 
 def Vq(q, I, I1, I2, I3, j, theta):
-    return 'V(q)'
+    k_p=k(I,I1,I2,I3,j,theta)
+    v_p=v0(I1,I1,I2,j,theta)
+    s_p=SN(q,k_p)
+    c_p=CN(q,k_p)
+    d_p=DN(q,k_p)
+    T1=(I*(I+1.0)*k_p*k_p+v_p*v_p)*s_p*s_p
+    T2=(2.0*I+1.0)*v_p*c_p*d_p
+    return T1+T2
+    
 
 
 qValues = np.arange(-8.0, 8.1, 0.5)
@@ -63,4 +103,16 @@ def CreatePotential(I, theta):
     potentials = [V, VChiral]
     return potentials
 
-# def PlotPotential(spin, theta):
+def PlotPotential(I,theta):
+    title='chiralPotential.pdf'
+    filename='../../out/'+title
+    V=CreatePotential(I,theta)
+    #V[0] is the normal triaxial potential, for a value of coupling angle theta
+    #V[1] is the potential for the chiral transformation on the nucleus, with the angle theta=theta+pi
+    plt.plot(qValues,V[0],'-b',label=f'$V$')
+    plt.plot(qValues,V[1],'-r',label=f'$V_c$')
+    plt.savefig(filename,bbox_inches='tight')
+
+
+PlotPotential(9.5,-80)
+# print(u(9.5,80, 100, 90, 5.5, 100))
