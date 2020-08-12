c17ceadaa666e15f66fb17feda4312f2a1d72f19
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-08-12 08:27:50 +0300

    initial python impl scheme

---
+++ code/python/rms/rms.py
@@ -0,0 +1,11 @@
+import numpy
+
+yrast_exp=[1,2]
+tw1_exp=[]
+tw2_exp=[]
+
+def Compute_RMS(exp_data,th_data):
+    for i in range(len(exp_data)):
+        print(i)
+
+Compute_RMS(yrast_exp,yrast_exp)
