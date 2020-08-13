1af07aa1cd874758fc49d5575869b8bc5d259b6f
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-08-13 14:57:55 +0300

    adds array with minimal values. fixes previous commit error

--- code/python/rms/rms.py
+++ code/python/rms/rms.py
@@ -1,6 +1,7 @@
 import numpy as np
 from timeit import default_timer as timer
 import time
+from numpy import random as rd
 
 params = [5.2, 1.9, 2.0, -0.1]
 data_exp = [[1, 9.], [2, 8.9], [3, 8.8], [
@@ -15,10 +16,8 @@ mois = np.arange(1.0, 6.1, 0.5)
 thetas = np.arange(-3.0, 3.0, 0.1)
 
 
-params_array = []
-
-
-def CreateParams(params_array):
+def CreateParams():
+    params_array=[]
     for i1 in mois:
         for i2 in mois:
             for i3 in mois:
@@ -28,17 +27,19 @@ def CreateParams(params_array):
     return params_array
 
 
-CreateParams(params_array)
+params_array=CreateParams()
 
 
 def GenerateTheoreticalData(exp_data, i1, i2, i3, th):
     th_data = []
-    for id in range(len(exp_data)):
-        fx = Fct(i1, i2, i3, th, exp_data[id][0])
-        data_pair = [exp_data[id][0], fx]
+    for id in exp_data:
+        fx = Fct(i1, i2, i3, th, id[0])
+        data_pair = [id[0], fx]
         th_data.append(data_pair)
     return th_data
 
+
+test_th_data=GenerateTheoreticalData(data_exp,params[0],params[1],params[2],params[3])
 
 def Compute_RMS(exp_data, th_data):
     if(len(exp_data) != len(th_data)):
@@ -52,9 +53,9 @@ def Compute_RMS(exp_data, th_data):
         term = np.power(diff, 2)
         if(term != np.nan):
             count = count+1
-        sum_t = sum_t+term
+            sum_t = sum_t+term
     if(count == len(exp_data)):
-        return round(np.sqrt(sum/len(exp_data)), 3)
+        return round(np.sqrt(sum_t/len(exp_data)),5)
     else:
         return np.nan
 
@@ -75,5 +76,15 @@ def GetMinRMS(params_array):
     print(f'Process Duration: {(end-start)} seconds')
     # print(params_array[rms_array.index(min(rms_array))])
 
+print(Compute_RMS(data_exp,test_th_data))
 
-GetMinRMS(params_array)
+# GetMinRMS(params_array)
+
+array_test=[]
+testarr=rd.rand(10)
+for x in testarr:
+    array_test.append(x)
+
+print(array_test)
+min_id=array_test.index(min(array_test))
+print(min_id,array_test[min_id])
